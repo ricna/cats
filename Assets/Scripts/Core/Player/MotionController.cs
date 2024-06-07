230bc6b9abbdbdc419dfa027662eb0c2cd175ed4
Ricna <ricna@unrez.com>
2024-06-06 23:34:28 -0300

    Cat using full Rigidbody! AddForce with MaxVelocity, Acceleration and Deceleration.

--- Assets/Scripts/Core/Player/MotionController.cs
+++ Assets/Scripts/Core/Player/MotionController.cs
@@ -37,13 +37,21 @@ namespace Unrez
 
         [Header("Settings")]
         [SerializeField]
-        private float _speed = 5;
+        [Tooltip("MaxVelocity. Force will be calculated to use with AddForce (_force = _maxVelocity * _rb.drag). Linear Drag should be > 0")]
+        private float _maxVelocity = 10;
+        [Tooltip("Linear Drag when IsMoving (Input != 0 )")]
+        [SerializeField]
+        private float _acceleration = 5;
+        [Tooltip("Linear Drag when !IsMoving (Input == 0)")]
+        [SerializeField]
+        private float _deceleration = 10;
         [SerializeField]
         private float _awayTime = 10;
-
         private Vector2 _movementInput;
 
         [Header("Debugs")]
+        [SerializeField]
+        private float _force;
         [SerializeField]
         private CatSide _catSide;
         [SerializeField]
@@ -52,81 +60,12 @@ namespace Unrez
         private bool _isMoving;
         private bool _isMovingVertical = false;
         private bool _isMovingHorizontal = false;
-        [SerializeField]
-        private float _velocity;
-        private Vector2 _posInit;
-        private Vector2 _posFinal;
-        private bool _checkVelocity = true;
+        private bool _lastDirX;
+        private bool _lastDirY;
 
-        private void FixedUpdate()
-        {
-            if (!IsOwner)
-            {
-                return;
-            }
-            Vector2 newPosition = Vector2.zero;
-            _isMovingHorizontal = _movementInput.x != 0;
-            _isMovingVertical = _movementInput.y != 0;
-            _isMoving = _isMovingHorizontal || _isMovingVertical;
-            if (_isMoving)
-            {
-                if (_isMovingVertical)
-                {
-                    newPosition = (Vector2)_transform.position + (Vector2.up * _movementInput.y);
-                    _catSide = _movementInput.y > 0 ? CatSide.CAT_UP : CatSide.CAT_DOWN;
-                }
-                if (_isMovingHorizontal)
-                {
-                    if (_isMovingVertical)
-                    {
-                        newPosition = (Vector2)_transform.position + (Vector2.up * _movementInput.y + Vector2.right * _movementInput.x);
-                    }
-                    else
-                    {
-                        newPosition = (Vector2)_transform.position + (Vector2.right * _movementInput.x);
-                    }
-                    _catSide = _movementInput.x > 0 ? CatSide.CAT_RIGHT : CatSide.CAT_LEFT;
-                }
-                if (_lastCatSide != _catSide && _catSide != CatSide.NO_SIDE)
-                {
-                    _lastCatSide = _catSide;
-                }
-                _rb.MovePosition(Vector2.Lerp(_transform.position, newPosition, _speed * Time.fixedDeltaTime));
-            }
-            else
-            {
-                _catSide = CatSide.NO_SIDE;
-            }
-            _animator.SetBool(AnimatorParameter.IS_MOVING, _isMoving);
-            _animator.SetFloat(AnimatorParameter.CAT_SIDE, (int)_lastCatSide);
-            if (_checkVelocity)
-            {
-                _checkVelocity = false;
-                if (_coroutineCheckVelocity != null)
-                {
-                    StopCoroutine(_coroutineCheckVelocity);
-                }
-                _coroutineCheckVelocity = StartCoroutine(CheckVelocity());
-            }
-        }
-        private Coroutine _coroutineCheckVelocity;
-        [SerializeField]
-        private float _time;
-        [SerializeField]
-        private float _distance;
-        private IEnumerator CheckVelocity()
-        {
-            _posInit = _transform.position;
-            _time = 1 * Time.deltaTime;
-            yield return new WaitForSeconds(_time);
-            _posFinal = _transform.position;
-            _distance = (_posInit - _posFinal).magnitude;
-            _velocity = _distance / _time;
-            //v = Δs / Δt, onde Δs é o deslocamento e Δt é o intervalo de tempo.
-            _checkVelocity = true;
-        }
         public override void OnNetworkSpawn()
         {
+
             if (!IsOwner)
             {
                 return;
@@ -153,6 +92,59 @@ namespace Unrez
                     _movementInput = movementInput;
                 }
             }
+        }
+        private void FixedUpdate()
+        {
+            if (!IsOwner)
+            {
+                return;
+            }
+            //Vector2 newPosition = Vector2.zero;
+            Vector2 dir = Vector2.zero;
+            _isMovingHorizontal = _movementInput.x != 0;
+            _isMovingVertical = _movementInput.y != 0;
+            _isMoving = _isMovingHorizontal || _isMovingVertical;
+            if (_isMoving)
+            {
+                _rb.drag = _acceleration;
+                _force = _maxVelocity * _rb.drag;
+                if (_isMovingVertical)
+                {
+                    //newPosition = (Vector2)_transform.position + (Vector2.up * _movementInput.y);
+                    dir = Vector2.up * _movementInput.y;
+                    _catSide = _movementInput.y > 0 ? CatSide.CAT_UP : CatSide.CAT_DOWN;
+                }
+                if (_isMovingHorizontal)
+                {
+                    if (_isMovingVertical)
+                    {
+                        //newPosition = (Vector2)_transform.position + (Vector2.up * _movementInput.y + Vector2.right * _movementInput.x);
+                        dir = Vector2.up * _movementInput.y + Vector2.right * _movementInput.x;
+
+                    }
+                    else
+                    {
+                        //newPosition = (Vector2)_transform.position + (Vector2.right * _movementInput.x);
+                        dir = Vector2.right * _movementInput.x;
+                    }
+                    _catSide = _movementInput.x > 0 ? CatSide.CAT_RIGHT : CatSide.CAT_LEFT;
+                }
+                if (_lastCatSide != _catSide && _catSide != CatSide.NO_SIDE)
+                {
+                    _lastCatSide = _catSide;
+                }
+                //_rb.MovePosition(Vector2.Lerp(_transform.position, newPosition, _speed * Time.fixedDeltaTime));
+                _rb.AddForce(_force * /*Time.fixedDeltaTime **/ dir, ForceMode2D.Force);
+            }
+            else
+            {
+                _rb.drag = _deceleration;
+                _force = _maxVelocity * _rb.drag;
+                _rb.AddForce(Vector2.zero, ForceMode2D.Force);
+                _catSide = CatSide.NO_SIDE;
+            }
+            _animator.SetBool(AnimatorParameter.IS_MOVING, _isMoving);
+            _animator.SetFloat(AnimatorParameter.CAT_SIDE, (int)_lastCatSide);
         }
     }
 }
